bd7dfdeb14876023b0af5cc0f29a0976a92dd28c
brauliogarcia-a <braulio.garcia.analyst@gmail.com>
2026-02-10 01:46:32 -0600

    First Commit MVC Model

---
+++ Programming_Languages_Python_JS_PHP_C#/MvMovie/Migrations/20260210051024_AddRatingToMovie.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingToMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 5,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
